3c6efd34ce6e1030030c8c64d6ead3f764b92ecd
Bigsam08 <uelsam994@gmail.com>
2024-03-13 14:18:32 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if (ord(letter) > 96 and ord(letter) <= 124):
+            letter = chr(ord(letter) - 32)
+        print("{}".format(letter), end='')
+    print()
