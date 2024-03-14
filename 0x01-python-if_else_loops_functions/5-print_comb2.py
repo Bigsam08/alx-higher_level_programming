d6d4c545d99357d441ff2d2a21d0162e9f50a894
Bigsam08 <uelsam994@gmail.com>
2024-03-14 02:57:09 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for digit in range(0, 100):
-    if digit <= 99:
-        print(f"{digit: 02}", end=",", "{\n}")
+for i in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
     else:
-        print("\n")
+        print("{:02}".format(i), end=', ')
