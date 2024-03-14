f6c839c59aacaab9a45e6f1cd7940b082f4fc0db
Bigsam08 <uelsam994@gmail.com>
2024-03-14 12:48:30 -0700

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    tray = dir(hidden_4)
+    for i in range(len(tray)):
+        if (tray[i][:2] != "__"):
+            print(tray[i])
