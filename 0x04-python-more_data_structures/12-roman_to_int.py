ff822f616cffb0fa9e2f82a74ef8af87de0875f5
Bigsam08 <uelsam994@gmail.com>
2024-04-16 05:35:00 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,11 +2,11 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
-    roman_fig = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_fig = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_fig[roman_string[i]] > roman_fig[roman_string[i - 1]]:
-            list += roman_fig[roman_string[i]] - 2 * roman_fig[roman_string[i - 1]]
+        if i > 0 and rom_fig[roman_string[i]] > rom_fig[roman_string[i - 1]]:
+            list += rom_fig[roman_string[i]] - 2 * rom_fig[roman_string[i - 1]]
         else:
-            list += roman_fig[roman_string[i]]
+            list += rom_fig[roman_string[i]]
     return list
