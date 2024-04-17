c725d6adc8fe242ea3e1b6c46b4cd2a9084a49db
Bigsam08 <uelsam994@gmail.com>
2024-04-17 01:33:05 -0700

    my new commit

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" Node class."""
+
+
+class Node:
+    """Defining Node linked list."""
+
+    def __init__(self, data, next_node=None):
+        """Initializing Node """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """data of the Node."""
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """set next_node of the Node."""
+        return (self.__next_node)
+
+    @next_node.setter
+    def next_node(self, value):
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Represent a singly-linked list."""
+
+    def __init__(self):
+        """Initalize a new SinglyLinkedList."""
+        self.__head = None
+
+    def sorted_insert(self, value):
+        """Insert a new Node to the SinglyLinkedList.
+
+        Args:
+            value (Node): new node to insert
+        """
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            current = self.__head
+            while (current.next_node is not None and
+                    current.next_node.data < value):
+                current = current.next_node
+            new.next_node = current.next_node
+            current.next_node = new
+
+    def __str__(self):
+        """print SinglyLinkedList."""
+        values = []
+        current = self.__head
+        while current is not None:
+            values.append(str(current.data))
+            current = current.next_node
+        return ('\n'.join(values))
