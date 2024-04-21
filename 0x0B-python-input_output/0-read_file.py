216fa096986eb0a7b02181f15e64e841abe67010
Bigsam08 <uelsam994@gmail.com>
2024-04-21 13:36:15 -0700

    new commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-A function that reads a file and
-print out its content to standard output
-"""
+""" A function that reads from a file """
 
 
 def read_file(filename=""):
+    """ Opens and read a file to stdout  """
     with open(filename, "r", encoding="utf-8") as new_file:
-        print(new_file.read(), end="")
+        print(new_file.read(), end='')
