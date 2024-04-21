a14e3b32661e315097934dbccdd247b26776f66d
Bigsam08 <uelsam994@gmail.com>
2024-04-21 13:27:42 -0700

    new commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that reads a file and print out its content to standard output"""
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as new_file:
+        print(new_file.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+Python - Input/Output
+Here I understood implementation of builtin with, open, and read functions with the json module to read and write files and serialize and deserialize objects with JSON.
