27e8dbdb2801641d215b645ce432ae2467856e64
Bigsam08 <uelsam994@gmail.com>
2024-04-21 14:22:24 -0700

    new commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """
-    Adding all arguments to a list
+    Adding argument list into a file named store
 """
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    store = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    store = []
-
-for ipp in sys.argv[1:]:
-    store.append(ipp)
-save_to_json_file(store, "add_item.json")
+    try:
+        store = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        store = []
+    store.extend(sys.argv[1:])
+    save_to_json_file(store, "add_item.json")
