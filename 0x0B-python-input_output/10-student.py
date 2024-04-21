9fd5d9fda42717fe1f73c0c2fd732dc7371660f4
Bigsam08 <uelsam994@gmail.com>
2024-04-21 14:36:48 -0700

    new commit

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" create a student class """
+
+
+class Student:
+    
+    def __init__(self, first_name, last_name, age):
+        """ class attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation """
+        if (type(attrs)) == list and all(type(i) == str for i in attrs):
+            return {nis: getattr(self, nis) for nis in attrs if hasattr(self, nis)}
+        else:
+            return self.__dict__
