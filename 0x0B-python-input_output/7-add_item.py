746b780317f9340aae9a44c09174256822e65b1a
Bigsam08 <uelsam994@gmail.com>
2024-04-23 00:36:43 -0700

    new commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" A function that adds all args to a list """
+"""Add  args to a  list and save them to a file."""
 import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    args_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    args_list = []
-
-for arg in sys.argv[1:]:
-    args_list.append(arg)
-save_to_json_file(args_list, "add_item.json")
+    try:
+        lst = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        lst = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(lst, "add_item.json")
