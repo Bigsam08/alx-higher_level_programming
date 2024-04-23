7fa4c4301f613db2f730d346e56e2405e2ef659d
Bigsam08 <uelsam994@gmail.com>
2024-04-23 01:27:11 -0700

    new commit

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""Reads from standard input,and at every ten lines
-or the input (CTRL + C),
-prints the following reports:
-    * Total file size up to that point.
-    * Count of read status codes up to that point.
+"""Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics."""
+    """Print accumulated metrics.
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
     print("File size: {}".format(size))
-    for k in sorted(status_codes):
-        print("{}: {}".format(k, status_codes[k]))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
