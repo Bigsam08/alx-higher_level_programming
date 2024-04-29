1116229ad7555d4bf74c5c8061e166ec4816ee13
Bigsam08 <uelsam994@gmail.com>
2024-04-29 03:24:16 -0700

    more classes project

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+"""Solves the N-queens puzzle.
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+Example:
+    $ ./101-nqueens.py N
+N must be an integer greater than or equal to 4.
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
+import sys
+
+
+def chess_board(n):
+    """An `r`x`c` sized chessboad"""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(" ") for i in range(n) for row in board]
+    return (board)
+
+
+def board_two(board):
+    """
+        A copy of the chessboard
+        named board_two
+    """
+    if isinstance(board, list):
+        return list(map(board_two, board))
+    return (board)
+
+
+def solve(board):
+    """
+        List of solved chessboard.
+        row and colum
+    """
+    solve = []
+    for colum in range(len(board)):
+        for row in range(len(board)):
+            if board[colum][row] == "Q":
+                solve.append([colum, row])
+                break
+    return (solve)
+
+
+def unmarked(board, row, col):
+    """
+        unmarked spots on a chessboard.
+        Spots Queen without attack can not move to on the board
+    """
+    # X out all forward spots
+    for c in range(col + 1, len(board)):
+        board[row][c] = "x"
+    # X out all backwards spots
+    for c in range(col - 1, -1, -1):
+        board[row][c] = "x"
+    # X out all spots below
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
+    # X out all spots above
+    for r in range(row - 1, -1, -1):
+        board[r][col] = "x"
+    # X out all spots diagonally down to the right
+    c = col + 1
+    for r in range(row + 1, len(board)):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally up to the left
+    c = col - 1
+    for r in range(row - 1, -1, -1):
+        if c < 0:
+            break
+        board[r][c]
+        c -= 1
+    # X out all spots diagonally up to the right
+    c = col + 1
+    for r in range(row - 1, -1, -1):
+        if c >= len(board):
+            break
+        board[r][c] = "x"
+        c += 1
+    # X out all spots diagonally down to the left
+    c = col - 1
+    for r in range(row + 1, len(board)):
+        if c < 0:
+            break
+        board[r][c] = "x"
+        c -= 1
+
+
+def recursive_solve(board, row, queens, solutions):
+    """Recursively solve an N-queens puzzle.
+    Args:
+        board (list): The current working chessboard.
+        row (int): The current working row.
+        queens (int): The current number of placed queens.
+        solutions (list): A list of lists of solutions.
+    Returns:
+        solutions
+    """
+    if queens == len(board):
+        solutions.append(solve(board))
+        return (solutions)
+
+    for c in range(len(board)):
+        if board[row][c] == " ":
+            tmp_board = board_two(board)
+            tmp_board[row][c] = "Q"
+            unmarked(tmp_board, row, c)
+            solutions = recursive_solve(tmp_board, row + 1,
+                                        queens + 1, solutions)
+
+    return (solutions)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = chess_board(int(sys.argv[1]))
+    solutions = recursive_solve(board, 0, 0, [])
+    for py in solutions:
+        print(py)
