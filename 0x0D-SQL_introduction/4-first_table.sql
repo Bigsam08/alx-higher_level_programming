8bc3694dbc774948888ff3525f001e781fcc3f20
Bigsam08 <uelsam994@gmail.com>
2024-05-21 15:59:44 -0700

    2&3

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- creates a table called first_table in the database of MYSQL server
+-- attributes include name and id
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
+    );
