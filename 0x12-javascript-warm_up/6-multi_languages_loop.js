701be5f791bd34d3d727a24305338bdd000fb080
Bigsam08 <uelsam994@gmail.com>
2024-06-20 09:17:42 -0700

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// using a loop to print 3 different msgs using only one print out function
+
+const words = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < words.length) {
+  console.log(words[i]);
+  i++;
+}
