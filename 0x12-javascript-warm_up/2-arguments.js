2e69628582e46227b4ec314f1f03f3eff2b1e8e5
Bigsam08 <uelsam994@gmail.com>
2024-06-20 07:11:18 -0700

    2-arguments.js

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// script that takes argument and returns msg if arg is passed or not
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
