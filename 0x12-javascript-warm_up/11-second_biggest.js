26d053d746e47949ee4490864e4b5ff4b2d5270a
Bigsam08 <uelsam994@gmail.com>
2024-06-20 14:59:40 -0700

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// look for the second biggest number in the argv list
+
+const arg = process.argv.slice(2).map(Number);
+
+if (arg.length === 0  || arg.length === 1) {
+  console.log('0');
+} else {
+  const biggest = Math.max(...arg);
+  const secondBiggest = Math.max(...arg.filter(x => x < biggest));
+  console.log(secondBiggest);
+}
