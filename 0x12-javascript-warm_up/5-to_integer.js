6d5eabffcc8d7b494e39c69da23bf09dda7f0acb
Bigsam08 <uelsam994@gmail.com>
2024-06-20 08:48:22 -0700

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// convert string to integer using '+' 'Number' 'math.floor' or 'parseInt'
+
+const arg = Math.floor(process.argv[2]);
+if (isNaN(arg)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ', arg);
+}
