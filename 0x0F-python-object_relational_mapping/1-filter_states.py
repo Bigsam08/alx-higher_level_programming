9222b19855a9440f821d1d83ecdc30a9487e11f7
Bigsam08 <uelsam994@gmail.com>
2024-06-22 07:04:58 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    A python MySQLdb script to connect to MySQL database
+    print all states starting with 'N' only
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    conet = MySQLdb.connect(
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            host='localhost',
+            port=3306
+            )
+    cur = conet.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    
+    data = cur.fetchall()
+    for r in data:
+        print(r)
+
+    cur.close()
+    conet.close()
