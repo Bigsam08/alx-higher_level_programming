e30681132e98e00b9b388a9de823c551d13f7138
Bigsam08 <uelsam994@gmail.com>
2024-06-22 07:20:37 -0700

    1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
             port=3306
             )
     cur = conet.cursor()
-    cur.execute("SELECT DISTINCT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     data = cur.fetchall()
     for r in data:
         print(r)
