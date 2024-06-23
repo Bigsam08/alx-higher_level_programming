419689a85f5dcc3c10cb8ab65213cdda9c8f36a9
Bigsam08 <uelsam994@gmail.com>
2024-06-23 09:34:33 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" script connects to base to search for states
+    using user formatted inout
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    conet = MySQLdb.connect(
+            user=sys.argv[1],
+            host='localhost',
+            port=3306,
+            password=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cur = conet.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
+    data = cur.fetchall()
+    for val in data:
+        print(val)
+    cur.close()
+    conet.close()
