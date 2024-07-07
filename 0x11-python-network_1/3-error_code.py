6c1d9cb67fd97003288b15226c63ee9170d4dcd9
Bigsam08 <uelsam994@gmail.com>
2024-07-07 09:12:48 -0700

    3 refix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ handling request errors in urllib """
+
 import urllib
 from urllib import request, error
 import sys
 
 
 if __name__ == '__main__':
-    url = argv[1]
+    url = sys.argv[1]
     try:
         with request.urlopen(url) as response:
             msg = response.read().decode('utf-8')
