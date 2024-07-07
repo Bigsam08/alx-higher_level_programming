29e8f2151423a4ef8d14c2e64d6b3a05db6690c3
Bigsam08 <uelsam994@gmail.com>
2024-07-07 08:15:07 -0700

    1 redo

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,7 @@ import urllib.request
 import sys
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         requested_id = response.info().get('X-Request-Id')
