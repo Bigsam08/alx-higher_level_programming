61e791acc21b6fe4ea61488ab78085a58e6c5a34
Bigsam08 <uelsam994@gmail.com>
2024-07-07 09:36:14 -0700

    task-4 refix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
-    print(F"\t- type: {type(response.text)}")
-    print(F"\t- content: {type(response.txt)}")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
