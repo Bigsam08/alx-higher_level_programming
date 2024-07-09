cc07591db09b2574bdc5055d2697e9f6d47e269c
Bigsam08 <uelsam994@gmail.com>
2024-07-09 04:35:42 -0700

    task 7 debug

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" a python script that uses a GET requests to send to a url
+"""
+    a python script that uses a GET requests to send to a url
     and printing back in display
     if status is greater or equal to 400 display error
 """
@@ -9,8 +10,9 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
+
+    response = requests.get(sys.argv[1])
+
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
