094107d3295052d474989fd46173b8895052c948
Bigsam08 <uelsam994@gmail.com>
2024-07-30 15:47:56 -0700

    get request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,5 +8,5 @@ request.get(url, (error, response, body) => {
     console.error(error);
     return;
   }
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
